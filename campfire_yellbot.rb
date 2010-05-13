2b58724e01386443230331b2c9d19f1ac7c12b88
mac <mac.s.c.chapman@gmail.com>
2010-05-13 14:31:31 +0100

    Config now includes regexes and uses arrays to print to campfire.  Also uses only one token

--- campfire_yellbot.rb
+++ campfire_yellbot.rb
@@ -4,36 +4,58 @@ require 'yaml'
 require 'broach'
 require 'twitter/json_stream'
 
+module Broach
+  class Session
+    def post_empty(path, payload)
+      response = REST.post(url_for(path), payload.to_json, headers_for(:post), credentials)
+      if response.created?
+        JSON.parse(response.body)
+      end
+    end
+  end
+
+  class Room
+    def join
+      Broach.session.post_empty("room/#{id.to_i}/join.xml", '')
+    end
+
+    def leave
+      Broach.session.post_empty("room/#{id.to_i}/leave.xml", '')
+    end
+  end
+end
 
 CONFIG = YAML.load_file('config.yml')
 REPLIES = YAML.load_file('replies.yml')
 
 def reply!(room, message)
-  case message
-#when /^rage$/ then room.speak "FFFFFUUUUU"
-    when /^rage$/ then respond(room, 'rage')
-    when /^ewbte$/ then respond(room, 'ewbte')
+  REPLIES.each_pair do |name, reply|
+    if Regexp.new(reply['regex']).match(message)
+      respond(room, reply)
+    end
   end
 end
-def respond(room, match)
-  room.speak(REPLIES[match]['message'].to_s)
-  if REPLIES[match].include? 'image'
-    room.speak(REPLIES[match]['image'])
+
+def respond(room, reply)
+  reply['message'].each do |message|
+    room.speak(message)
   end
 end
 
 options = {
   :path => "/room/#{CONFIG['room_id']}/live.json",
   :host => 'streaming.campfirenow.com',
-  :auth => "#{CONFIG['listen_token']}:x"
+  :auth => "#{CONFIG['token']}:x"
 }
 
 Broach.settings = {
   'account' => CONFIG['account'],
-  'token'   => CONFIG['speak_token'],
+  'token'   => CONFIG['token'],
   'use_ssl' => false
 }
 room = Broach::Room.find CONFIG['room_id']
+
+room.join
 
 EventMachine::run do
   stream = Twitter::JSONStream.connect(options)
